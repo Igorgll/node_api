c49ad9313607e6c5bc488edc09d1dca0932f1f5a
igorgll <igorlimagn@gmail.com>
2022-10-07 01:09:48 -0300

    @igorgll [Heroku Deploy]

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import routes from './routes/routes';
 const app = express()
 
 //Settings
-app.set('port', config.port)
+// app.set('port', config.port)
+app.listen(process.env.PORT || 3000);
 
 //middleware
 app.use(express.json())
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from "./app";
 
 app.listen(app.get("port"));
 
-console.log("Server on port", app.get("port"));
+console.log("Server on port", "3000");
